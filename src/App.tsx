3ff21248f1b2ebf304b430430579c336b84c506d
Tomasz Prochowicz <t.prochowicz@selleo.com>
2024-05-14 16:56:52 +0200

    feat: add auth logic

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { Route, Routes } from "react-router-dom";
+import "./globals.css";
+
+import { Home } from "./_root/pages";
+
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import SignupForm from "./_auth/forms/SignupForm";
+import SigninForm from "./_auth/forms/SigninForm";
+import { Toaster } from "./components/ui/toaster";
+
+const App = () => {
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+
+      <Toaster />
+    </main>
+  );
+};
+
+export default App;
